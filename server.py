ea6f797ad8d7855d8a247fe15e6c423616c44c58
Hannah Johnson <hstutzman@gmail.com>
2019-05-14 06:29:57 +0000

    Saving server.py

--- server.py
+++ server.py
@@ -8,7 +8,7 @@ from jinja2 import StrictUndefined
 from flask import Flask, render_template, redirect, request, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from model import connect_to_db, db, Facility, Visitation, 
-                    Citation, 
+                    Citation, CitationDefinition
 
 ##### Create App #############################################################
 
