4552245038be5c6c6c1476687bf6b801d5c31f98
Hannah Johnson <hstutzman@gmail.com>
2019-05-22 07:29:47 +0000

    updated filter.html, worked on date filter in server.py

--- server.py
+++ server.py
@@ -9,6 +9,7 @@ from flask import Flask, render_template, redirect, request, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from model import connect_to_db, db, Facility, Visitation, Citation, CitationDefinition
 from sqlalchemy import func
+from dateutil.parser import *
 
 ##### Create App #############################################################
 
@@ -44,40 +45,29 @@ def process_form():
     min_cit = request.form.get('min_cit')
     max_cit = request.form.get('max_cit')
     status = request.form.get('status').upper()
-    date = request.form.get('date')
-    ### TODO - Add status filter
-    ### TODO - Add date-range filter
+    max_date = request.form.get('min_date') ### more recent
+    min_date = request.form.get('max_date') ### less recent
+       ### TODO - add filter logic
 
-    print(">>>>>>>>>>>>>>>>>>>>>>>>", date)
-    ### TODO - Turn into datetime obj
+    if name or zipcode or min_cit or max_cit or status or min_date:
 
-    if name or zipcode or min_cit or max_cit or status:
-
-        fquery = Facility.query
+        fquery = Facility.query ### Base query
 
         if name:
             fquery = fquery.filter(Facility.facility_name.like(f'%{name}%'))
+        
         if zipcode:
             fquery = fquery.filter(Facility.facility_zip == int(zipcode))
 
-        #####################################################################################
-        ### Below queries need to be like:
-
-        ### SELECT facilities.facility_name, citations.facility_id, COUNT(*) 
-        ### FROM citations JOIN facilities ON 
-        ### facilities.facility_id = citations.facility_id 
-        ### GROUP BY (citations.facility_id, facilities.facility_id);
-
-
-        ### SELECT COUNT(citation_id), facility_id FROM citations GROUP BY facility_id;
         if min_cit:
-
+            ### Below query based off of https://stackoverflow.com/a/38639550
             fquery = (fquery
                 .outerjoin(Facility.citations)
                 .group_by(Facility)
                 .having(func.count_(Facility.citations) >= min_cit))
 
         if max_cit:
+            ### Below query based off of https://stackoverflow.com/a/38639550
             fquery = (fquery
                 .outerjoin(Facility.citations)
                 .group_by(Facility)
@@ -86,32 +76,71 @@ def process_form():
         if status:
             fquery = fquery.filter(Facility.facility_status == status)
 
-        facilities = fquery.all()
+        if min_date:
+            # SELECT * FROM citations WHERE citation_date BETWEEN '2017-01-01' AND '2019-05-01';
+            # can replace right date w/ "current_date" or left date with "where time > XXXdateXXX()"
+            # https://popsql.com/learn-sql/postgresql/how-to-query-date-and-time-in-postgresql/
 
 
-#######################
+            ### THIS WORKED
+            ### SELECT facilities.facility_name, citations.citation_date
+            ### FROM facilities
+            ### LEFT JOIN citations
+            ### ON facilities.facility_id = citations.facility_id
+            ### GROUP BY (citations.citation_date, facilities.facility_id)
+            ### ORDER BY facilities.facility_id;
 
-            # facilities = (Facility.query
-            #     .(querystring)
-            #     # .outerjoin(Facility.citations)
-            #     # .group_by(Facility)
-            #     # .having(func.count_(Facility.citations) >= min_cit)
-            #     .all())
+            ### MOST RECENT CITATION, Based on citations table=
+            # test1=# SELECT facilities.facility_name, facilities.facility_id, MAX(citations.citation_date)
+            # test1-# FROM citations
+            # test1-# LEFT JOIN facilities
+            # test1-# ON facilities.facility_id = citations.facility_id
+            # test1-# GROUP BY (facilities.facility_id)
+            # test1-# ORDER BY facilities.facility_id;
 
-#######################
+            ### MOST RECENT CITATION, based on facilities table
+            # test1=# SELECT facilities.facility_name, facilities.facility_id, MAX(citations.citation_date)
+            # test1-# FROM facilities 
+            # test1-# LEFT JOIN citations
+            # test1-# ON facilities.facility_id = citations.facility_id
+            # test1-# GROUP BY facilities.facility_id
+            # test1-# ORDER BY facilities.facility_id;
 
+            ### MOST RECENT CITATION W/ FILTER DATE
+            # SELECT DISTINCT ON (facilities.facility_id) facilities.facility_id, MAX(citations.citation_date)
+            # FROM facilities 
+            # LEFT JOIN citations
+            # ON facilities.facility_id = citations.facility_id
+            # GROUP BY facilities.facility_id, citations.citation_date HAVING citations.citation_date > '2018-01-01'
+            # ORDER BY facilities.facility_id;
+            isoparse(min_date)
+            
+            ### TODO - Fix this query
+            fquery = (fquery
+                .outerjoin(Facility.citations)
+                .group_by(Facility.facility_id, Facility.citations)
+                .having(func.and_(Facility.citations <= min_date))
+                )
+            #10503
+            
+            #current_date = datetime.datetime.utcnow()
+            #date_results = Citation.query.filter(Citation.citation_date <= min_date)
+            # fquery = (fquery
+            #     .outerjoin(Facility.citations)
+            #     .group_by(Facility)
+            #     .having(Facility.citations.citation_date <= isoparse(min_date)))
 
-        flash('Applying your requested filters now!')
+        facilities = fquery.all() ### Conglomerate all applicable queries
+
+        flash('Applying your requested filters now...')
 
         return render_template('filter-results.html', facilities=facilities) 
-        ### TODO - figure out why it is not rendering test.html
         ### TODO - figure out how to diplay map w/ filtered points
     
     else:
         flash('No filters were applied.')
 
         return redirect('/') 
-
 
 
 @app.route('/filter_by_zip')
