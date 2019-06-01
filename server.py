0c5f62d228940b7a1cf4b73a66bf39148c0d41df
Hannah Johnson <hstutzman@gmail.com>
2019-06-01 00:03:12 +0000

    Created some logic in map.html js for different colored markers according to citation count

--- server.py
+++ server.py
@@ -39,17 +39,73 @@ def display_filter_form():
     return render_template('filter.html')
 
 
+@app.route('/facilities')
+def show_facilities():
+    """Facilities page"""
+
+    facilities = Facility.query.order_by(Facility.name).all()
+
+    return render_template('facilities.html', facilities=facilities)
+
+
+@app.route('/facilities/<f_id>')
+def show_facility_details(f_id):
+    """Facility details info page"""
+
+    facility = Facility.query.filter_by(f_id=f_id).one()
+
+    f_url = f'https://secure.dss.ca.gov/CareFacilitySearch/FacDetail/{facility.number}'
+
+    return render_template('/facility_profile.html', facility=facility, f_url=f_url)
+
+
+@app.route('/map')
+def show_map():
+    """Return page with facilities plotted to map
+    Data for base map pins coming from @app.route('/mapping-facilities.json')
+    Data for filtered map pins coming from 
+    """
+
+    return render_template('map.html')
+
+
+@app.route('/mapping-facilities.json')
+def create_map_json():
+    """Create json for base map out of facilities query"""
+
+    facilities_list = Facility.query.filter(Facility.longitude != None, Facility.status == 'LICENSED').all() 
+
+    facilities = {}
+    
+    for facility in facilities_list:     
+        mapinfo = {
+                    "title": facility.name,
+                    "lat": facility.latitude, 
+                    "lng": facility.longitude,
+                    "status": facility.status,
+                    "citation_count": len(facility.citations),
+                    }
+### TODO - This area is taking a lot of time to load, consider whether to add count to db, or some other solution
+        facilities[facility.f_id] = mapinfo
+    
+    return jsonify(facilities)
+
+
 @app.route('/filter-results.json')
 def process_form():
-    """Recieve and store filtration input"""
+    """Recieve and store filtration input into JSON"""
+    status = 'LICENSED'
 
     name = request.args.get('name').upper()
     zipcode = request.args.get('zipcode')
     min_cit = request.args.get('min_cit')
     max_cit = request.args.get('max_cit')
-    status = request.args.get('status').upper()
+    if request.args.get('status').upper():
+        status = request.args.get('status').upper()
     suppress_date = request.args.get('suppress_date')
     f_type = request.args.get('type')
+
+    ### TODO - Citation date and count may not be working together
 
     if name or zipcode or min_cit or max_cit or status or suppress_date or f_type:
 
@@ -94,7 +150,7 @@ def process_form():
                 f_type = 'INFANT CENTER'
             elif f_type == 'ill':
                 f_type = 'DAY CARE CENTER - ILL CENTER'
-            elif f_type == 'day':
+            elif f_type == 'prek':
                 f_type = 'DAY CARE CENTER'
             elif f_type == 'school':
                 f_type = 'SCHOOL AGE DAY CARE CENTER'
@@ -104,8 +160,6 @@ def process_form():
 
         facility_count = len(facilities)
 
-        flash('Applying your requested filters now...')
-
         facilities_dict = {}
     
         for facility in facilities:     
@@ -114,142 +168,71 @@ def process_form():
                         "lat": facility.latitude, 
                         "lng": facility.longitude,
                         "status": facility.status,
+                        "citation_count": len(facility.citations),
                         }
+### TODO - This area is taking a lot of time to load, consider whether to add count to db, or some other solution
 
             facilities_dict[facility.f_id] = mapinfo
         
         return jsonify(facilities_dict)
-
-
-        # return render_template('filter-results.html', 
-        #                         facilities=facilities, 
-        #                         facility_count=facility_count)
-        
-        
-        # return render_template('map.html',
-        #                         create_map_json(facilities),
-        #                         facility_count=facility_count,
-        #                         ) 
-                                #fquery=fquery,
-
-
-
-            ### TODO - figure out how to diplay map w/ filtered points
     
     else:
-        flash('No filters were applied.')
-        ### TODO - Check to see if flash still populates page 
+
 
         return jsonify({}) 
 
 
-@app.route('/filter-results')
-def render_map():
-    """Test route to see if I can send results to map from same route"""
-
-    return render_template('map.html')
-
-
-@app.route('/facilities')
-def show_facilities():
-    """Facilities page"""
-
-    facilities = Facility.query.all()
-
-    return render_template('facilities.html', facilities=facilities)
-
-
-@app.route('/facilities/<f_id>')
-def show_facility_details(f_id):
-    """Facility details info page"""
-
-    facility = Facility.query.filter_by(f_id=f_id).one()
-
-    f_url = f'https://secure.dss.ca.gov/CareFacilitySearch/FacDetail/{facility.number}'
-
-    return render_template('/facility_profile.html', facility=facility, f_url=f_url)
-
-
-@app.route('/map')
-def show_map():
-    """Return page with facilities plotted to map
-    Data for map pins coming from @app.route('/mapping-facilities.json')
-    """
-
-    return render_template('map.html')
-
-
-@app.route('/mapping-facilities.json')
-def create_map_json():
-    """Create json for map out of facilities query"""
-
-    facilities_list = Facility.query.filter(Facility.longitude != None, Facility.status == 'LICENSED').all() 
-    ### TODO - add on a sort
-    #facilities = query_params.all()
-
-    facilities = {}
-    
-    for facility in facilities_list:     
-        mapinfo = {
-                    "title": facility.name,
-                    "lat": facility.latitude, 
-                    "lng": facility.longitude,
-                    "status": facility.status,
-                    }
-
-        facilities[facility.f_id] = mapinfo
-    
-    return jsonify(facilities)
-
+##### For Geocoding ##########################################################
 
 @app.route('/geocode-request', methods=['POST', 'GET'])
 def send_geocode_request():
     """Requesting of geocodes via Google Geocode API for all facilities in db.
     Also adds latitude, longitude, and Google Place ID to db.
     """
+    pass ### Only for use in finding and storing lat/long when adding/updating
 
-    facilities = Facility.query.filter(Facility.longitude == None).all()
+    # facilities = Facility.query.filter(Facility.longitude == None).all()
 
-    completed = []
-    failed = []
-    counter = 0
+    # completed = []
+    # failed = []
+    # counter = 0
 
-    for facility in facilities:
-        if counter <= 5500:
-            if '#' in facility.address:
-                address = ''
-                for charac in facility.address:
-                    if charac != '#':
-                        address += charac
-                    else: 
-                        break
-            else:
-                address = facility.address 
+    # for facility in facilities:
+    #     if counter <= 5500:
+    #         if '#' in facility.address:
+    #             address = ''
+    #             for charac in facility.address:
+    #                 if charac != '#':
+    #                     address += charac
+    #                 else: 
+    #                     break
+    #         else:
+    #             address = facility.address 
 
-            geocode_url = (f"https://maps.googleapis.com/maps/api/geocode/json?address={address}+{facility.city}+{facility.state}+{facility.f_zip}&key=AIzaSyAw0meNSqLUJr9iQ0JLsC0b0xXxwBLrP_U")
-            results = requests.get(geocode_url)
-            results = results.json()
+    #         geocode_url = (f"https://maps.googleapis.com/maps/api/geocode/json?address={address}+{facility.city}+{facility.state}+{facility.f_zip}&key=AIzaSyAw0meNSqLUJr9iQ0JLsC0b0xXxwBLrP_U")
+    #         results = requests.get(geocode_url)
+    #         results = results.json()
 
-            if len(results['results']) != 0:
-                answer = results['results'][0]
-                facility.latitude = answer.get('geometry').get('location').get('lat')
-                facility.longitude = answer.get('geometry').get('location').get('lng')
-                facility.google_place_id = answer.get("place_id")
-                #print('>>>>>>>>>>>>>', facility.longitude)
-                db.session.commit()
-                completed.append(facility.f_id)
+    #         if len(results['results']) != 0:
+    #             answer = results['results'][0]
+    #             facility.latitude = answer.get('geometry').get('location').get('lat')
+    #             facility.longitude = answer.get('geometry').get('location').get('lng')
+    #             facility.google_place_id = answer.get("place_id")
+    #             #print('>>>>>>>>>>>>>', facility.longitude)
+    #             db.session.commit()
+    #             completed.append(facility.f_id)
 
-            else:
-                completed.append('FAILED')
-                failed.append(facility.f_id)
-            if len(completed)%100 == 0:
-                print (">>>>>>>>> completed: ",len(completed), "failed: ", len(failed))
+    #         else:
+    #             completed.append('FAILED')
+    #             failed.append(facility.f_id)
+    #         if len(completed)%100 == 0:
+    #             print (">>>>>>>>> completed: ",len(completed), "failed: ", len(failed))
             
-            counter += 1
+    #         counter += 1
 
-    output = {'completed': completed, 'failed': failed}
+    # output = {'completed': completed, 'failed': failed}
 
-    return render_template('geocode-request.html', output=output)
+    # return render_template('geocode-request.html', output=output)
 
 
 ##### Dunder Main ############################################################
